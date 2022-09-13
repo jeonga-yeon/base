296a8172667cbb81e301122c3b5cbe6a71d14f28
joyfuldev <wings2132@naver.com>
2022-09-13 21:38:50 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("mongoDB connected"))
+  .catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
