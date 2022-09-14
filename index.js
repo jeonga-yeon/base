6a2657b4dccd4db9ff651cb5a4abbea4c241ebb1
joyfuldev <wings2132@naver.com>
2022-09-14 17:53:10 +0900

    config

--- index.js
+++ index.js
@@ -3,15 +3,35 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("mongoDB connected"))
+  .connect(config.mongoURI)
+  .then(() => console.log("mongoDB connected..."))
   .catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello World");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
